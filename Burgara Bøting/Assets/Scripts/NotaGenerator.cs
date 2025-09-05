60842f527c3a68ab9d6fdf20938342ad29ed40a6
HemmingP <hemming_lol@hotmail.com>
2025-09-05 23:55:34 +0100

    Node generation base complete.. now the lines are missing

--- Burgara Bøting/Assets/Scripts/NotaGenerator.cs
+++ Burgara Bøting/Assets/Scripts/NotaGenerator.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class NotaGenerator : MonoBehaviour
@@ -7,18 +8,23 @@ public class NotaGenerator : MonoBehaviour
 
     public GameObject botNode;
 
+    private List<List<GameObject>> botNodes2D = new List<List<GameObject>>();
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         Vector2 centerPos = transform.position;
-
+        int hIndex = 0;
         for(float h = 0; h <= height; h += 0.5f)
         {
-            for(float w = 0; w <= width; w += 0.5f)
+            botNodes2D.Add(new List<GameObject>());
+            float innerOffset = hIndex%2 == 0 ? 0.25f:0;
+            for(float w = 0; w <= width - innerOffset; w += 0.5f)
             {
-
-                // GameObject newBotNode = Instantiate(botNode, centerPos);
+                GameObject newBotNode = Instantiate(botNode, centerPos + new Vector2(-(width/2)+w+innerOffset,-(height/2)+h), Quaternion.identity);
+                botNodes2D[hIndex].Add(newBotNode);
             }
+            hIndex++;
         }
 
     }
