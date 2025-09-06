b90fc9d863979d3542673cd2698637a9c71faf4a
Ma-a-a-rni <marnifalkvard@gmail.com>
2025-09-06 17:06:51 +0100

    Input field inn start scene

---
+++ Burgara Bøting/Assets/Scripts/InputFieldHandler.cs
@@ -0,0 +1,7 @@
+using TMPro;
+using UnityEngine;
+
+public class InputFieldHandler : MonoBehaviour
+{
+    public TMP_InputField myInputField;
+}
--- Burgara Bøting/Assets/Scripts/LoadScene.cs
+++ Burgara Bøting/Assets/Scripts/LoadScene.cs
@@ -1,8 +1,29 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class LoadScene : MonoBehaviour
 {
+    public TMP_InputField myInputField;
+
+    void Start()
+    {
+        // Subscribe to the OnEndEdit event
+        myInputField.onEndEdit.AddListener(ProcessInput);
+    }
+
+    void ProcessInput(string inputText)
+    {
+        Debug.Log("User entered: " + inputText);
+        SetInputFieldText(inputText);
+    }
+
+    // Example of setting text programmatically
+    public void SetInputFieldText(string newText)
+    {
+        myInputField.text = newText;
+    }
+
     public void LoadTheScene(string scene)
     {
         SceneManager.LoadScene(scene);
